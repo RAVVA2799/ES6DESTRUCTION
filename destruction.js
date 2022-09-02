515c7246f4f1fa48e48d1f9af5d1dfadcf40ee5a
RAVVA2799 <80970669+RAVVA2799@users.noreply.github.com>
2022-09-02 11:46:29 +0530

    Add files via upload

---
+++ destruction.js
@@ -0,0 +1,10 @@
+// we have an array with the name and surname
+let arr = ["John", "Smith"]       // 2 VALUES IN AN ARRAY
+
+// destructuring assignment
+// sets firstName = arr[0]
+// and surname = arr[1]
+let [firstName, surname] = arr;       //VALUES ASSIGNED IN NAMES.
+
+console.log(firstName); // John      //JOHN &SMITH WILL BE
+console.log(surname);  // Smith     //SEPATERLY PRINTED
